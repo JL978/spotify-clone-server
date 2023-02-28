0ba9ec2425abb282c39b53ae7634601eb9cb8693
Harry Shindika <harryshindika@gmail.com>
2023-02-28 13:28:04 -0500

    Creating social tab components

--- client/src/App.js
+++ client/src/App.js
@@ -137,7 +137,7 @@ function App() {
                 <NavList>
                   <NavItem to='/' exact={true} name='Home' label='Home' />
                   <NavItem to='/search' exact={true} name='Search' label='Search' />
-                  <NavItem to='/social' exact={true} name='Social' label='Social' /> 
+                  <NavItem to='/social' exact={false} name='Social' label='Social' /> 
                   <NavItem to='/collection' exact={false} name='Library' label='Your Library' data_tip='library' data_for='tooltip' data_event='click' style={{ pointerEvents: loggedIn? 'auto':'none'}}/>
                 </NavList>
                 <PlayLists 
--- client/src/components/pages-components/SocialPage.js
+++ client/src/components/pages-components/SocialPage.js
@@ -1,1 +1,16 @@
 import React from "react";
+import PageTitle from "../featured-components/PageTitle";
+export default function SocialPage() {
+
+
+    return (
+        <>
+        <div className="social-page-content">
+            <div className="SocialPage">
+            <PageTitle name='Browse All' />
+                hi
+            </div>
+        </div>
+        </>
+    )
+}
