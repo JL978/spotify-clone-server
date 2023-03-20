f8a5ea2adf946e3508a806a7812407a3bf311923
Victor Del Carpio <victor.delcarpiogomez@yale.edu>
2023-03-19 20:11:10 -0400

    robust in case tracks does not exist, still needs to be patched

--- client/src/components/featured-components/TrackList.js
+++ client/src/components/featured-components/TrackList.js
@@ -2,19 +2,24 @@ import React from 'react'
 import TrackListItem from './TrackListItem'
 
 const TrackList = React.forwardRef(({tracks, styleName, highlight, playContextTrack}, ref) => {
-    return (
-        <div className="trackListContainer">
-            <ol className="trackList">
-                {tracks.map((track, index) => {
-                    if (index+1 < tracks.length){
-                        return <TrackListItem track={track} key={track.id} styleName={styleName} highlight={track.id === highlight} playContextTrack={playContextTrack}/>
-                    }else{
-                        return <TrackListItem ref={ref} track={track} key={track.id} styleName={styleName} highlight={track.id === highlight} playContextTrack={playContextTrack}/>
-                    }
-                })}
-            </ol>
-        </div>
-    )
+    if (!tracks) {
+        return <div></div>
+    } else {
+        return (
+            <div className="trackListContainer">
+                <ol className="trackList">
+                    {tracks.map((track, index) => {
+                        if (index+1 < tracks.length){
+                            return <TrackListItem track={track} key={track.id} styleName={styleName} highlight={track.id === highlight} playContextTrack={playContextTrack}/>
+                        }else{
+                            return <TrackListItem ref={ref} track={track} key={track.id} styleName={styleName} highlight={track.id === highlight} playContextTrack={playContextTrack}/>
+                        }
+                    })}
+                </ol>
+            </div>
+        )
+    }
+    
 })
 
 
