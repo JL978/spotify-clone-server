90b1d7b89b349b56d2b2745198e4771c4124ce0d
Victor Del Carpio <victor.delcarpiogomez@yale.edu>
2023-03-22 21:34:45 -0400

    skips over null tracks (this happens when playlists include leaks)

--- client/src/components/featured-components/TrackList.js
+++ client/src/components/featured-components/TrackList.js
@@ -9,11 +9,14 @@ const TrackList = React.forwardRef(({tracks, styleName, highlight, playContextTr
             <div className="trackListContainer">
                 <ol className="trackList">
                     {tracks.map((track, index) => {
-                        if (index+1 < tracks.length){
-                            return <TrackListItem track={track} key={track.id} styleName={styleName} highlight={track.id === highlight} playContextTrack={playContextTrack}/>
-                        }else{
-                            return <TrackListItem ref={ref} track={track} key={track.id} styleName={styleName} highlight={track.id === highlight} playContextTrack={playContextTrack}/>
+                        if (track) {
+                            if (index+1 < tracks.length){
+                                return <TrackListItem track={track} key={track.id} styleName={styleName} highlight={track.id === highlight} playContextTrack={playContextTrack}/>
+                            }else{
+                                return <TrackListItem ref={ref} track={track} key={track.id} styleName={styleName} highlight={track.id === highlight} playContextTrack={playContextTrack}/>
+                            }
                         }
+                        
                     })}
                 </ol>
             </div>
