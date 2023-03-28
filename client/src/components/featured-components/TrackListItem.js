7eb76298650767e24fd5e24526d8e85e5d6f66c6
Victor Del Carpio <victor.delcarpiogomez@yale.edu>
2023-03-27 22:55:37 -0400

    fixes infinite scroll in liked songs

--- client/src/components/featured-components/TrackListItem.js
+++ client/src/components/featured-components/TrackListItem.js
@@ -1,131 +1,93 @@
-import React, { useContext } from "react";
-import Icon from "../icons";
-import msTimeFormat from "../../utilities/utils";
-import { PlayContext } from "../../utilities/context";
+import React, { useContext } from 'react';
+import Icon from '../icons';
+import msTimeFormat from '../../utilities/utils';
+import { PlayContext } from '../../utilities/context';
 
-const TrackListItem = React.forwardRef(
-	({ track, styleName, highlight, playContextTrack }, ref) => {
-		const { album, artists, name, explicit, duration_ms, uri } = track;
-		const updatePlayer = useContext(PlayContext);
+const TrackListItem = React.forwardRef(({ track, styleName, highlight, playContextTrack }, ref) => {
+  const { album, artists, name, explicit, duration_ms, uri } = track;
+  const updatePlayer = useContext(PlayContext);
 
-		let thumbNail;
-		if (styleName === "simplify" && album.images.length > 0) {
-			thumbNail = album.images[album.images.length - 1].url;
-		}
-		const formattedTime = msTimeFormat(duration_ms);
-		return (
-			<li
-				ref={ref}
-				className={`trackListItem ${highlight ? "highlight" : null}`}
-			>
-				<div
-					className="trackItemPlay"
-					style={styleName === "simplify" ? simplyStyle : null}
-				>
-					<button
-						className={
-							styleName === "simplify"
-								? "hoverIcon no-outline"
-								: "hoverIcon trackTopAlign no-outline"
-						}
-						onClick={() => {
-							playContextTrack(uri);
-							updatePlayer();
-						}}
-					>
-						<Icon name="Play" height="20" width="20" />
-					</button>
-					<div
-						className={
-							styleName === "simplify" ? "itemIcon" : "itemIcon trackTopAlign"
-						}
-						style={{ marginTop: styleName === "simplify" ? "0" : null }}
-					>
-						<Icon name="Music" />
-					</div>
-				</div>
+  const thumbNail = styleName === 'simplify' && album.images.length > 0 ? album.images[album.images.length - 1].url : null;
+  const formattedTime = msTimeFormat(duration_ms);
+  const simplifyStyle = styleName === 'simplify';
 
-				{styleName === "simplify" && (
-					<div className="trackMidAlign">
-						<div className="trackItemThumb">
-							{thumbNail ? (
-								<img
-									loading="lazy"
-									src={thumbNail}
-									style={{ width: "100%", height: "100%" }}
-									alt=""
-								/>
-							) : (
-								<div
-									style={{
-										position: "absolute",
-										top: "35%",
-										bottom: "35%",
-										left: "35%",
-										right: "35%",
-									}}
-								>
-									<Icon name="Music2" />
-								</div>
-							)}
-						</div>
-					</div>
-				)}
+  const renderThumbnail = () => (
+    thumbNail ? (
+      <img
+        loading='lazy'
+        src={thumbNail}
+        style={{ width: '100%', height: '100%' }}
+        alt=''
+      />
+    ) : (
+      <div style={{ position: 'absolute', top: '35%', bottom: '35%', left: '35%', right: '35%' }}>
+        <Icon name='Music2' />
+      </div>
+    )
+  );
 
-				<div className="trackItemInfo">
-					<div
-						className={
-							styleName === "simplify" ? "trackMidAlign" : "trackTopAlign"
-						}
-					>
-						<div className="trackName ellipsis-one-line">{name}</div>
+  const renderTrackInfo = () => (
+    styleName !== 'simplify' && (
+      <div className='trackInfo'>
+        {explicit && <span className='explicit-label'>E</span>}
+        <span className='trackArtists ellipsis-one-line'>
+          {artists.map((artist) => (
+            <a href={`/artist/${artist.id}`} key={artist.id}>
+              {artist.name}
+            </a>
+          ))}
+        </span>
+        {album && (
+          <>
+            <span className='trackInfoSep'>•</span>
+            <span className='trackAlbum ellipsis-one-line'>
+              <a href={`/ablum/${album.id}`}>{album.name}</a>
+            </span>
+          </>
+        )}
+      </div>
+    )
+  );
 
-						{styleName !== "simplify" && (
-							<div className="trackInfo">
-								<span
-									className="explicit-label"
-									style={explicit ? { display: "flex" } : { display: "none" }}
-								>
-									E
-								</span>
-								<span className="trackArtists ellipsis-one-line">
-									{artists.map((artist) => (
-										<a href={`/artist/${artist.id}`} key={artist.id}>
-											{artist.name}
-										</a>
-									))}
-								</span>
-								{album && (
-									<>
-										<span className="trackInfoSep">•</span>
-										<span className="trackAlbum ellipsis-one-line">
-											<a href={`/ablum/${album.id}`}>{album.name}</a>
-										</span>
-									</>
-								)}
-							</div>
-						)}
-					</div>
-				</div>
+  return (
+    <li ref={ref} className={`trackListItem ${highlight ? 'highlight' : ''}`}>
+      <div className='trackItemPlay' style={simplifyStyle ? simplyStyle : null}>
+        <button className={`hoverIcon ${simplifyStyle ? 'no-outline' : 'trackTopAlign no-outline'}`} onClick={() => { playContextTrack(uri); updatePlayer(); }}>
+          <Icon name='Play' height='20' width='20' />
+        </button>
+        <div className={`itemIcon ${simplifyStyle ? '' : 'trackTopAlign'}`} style={{ marginTop: simplifyStyle ? '0' : null }}>
+          <Icon name='Music' />
+        </div>
+      </div>
 
-				<div className="trackItemDuration">
-					<div
-						className={`duration ${
-							styleName === "simplify" ? "trackMidAlign" : "trackTopAlign"
-						}`}
-					>
-						<span>{formattedTime}</span>
-					</div>
-				</div>
-			</li>
-		);
-	}
-);
+      {simplifyStyle && (
+        <div className='trackMidAlign'>
+          <div className='trackItemThumb'>
+            {renderThumbnail()}
+          </div>
+        </div>
+      )}
+
+      <div className='trackItemInfo'>
+        <div className={simplifyStyle ? 'trackMidAlign' : 'trackTopAlign'}>
+          <div className='trackName ellipsis-one-line'>{name}</div>
+          {renderTrackInfo()}
+        </div>
+      </div>
+
+      <div className='trackItemDuration'>
+        <div className={`duration ${simplifyStyle ? 'trackMidAlign' : 'trackTopAlign'}`}>
+          <span>{formattedTime}</span>
+        </div>
+      </div>
+    </li>
+  );
+});
 
 const simplyStyle = {
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "flex-end",
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
 };
 
 export default TrackListItem;
