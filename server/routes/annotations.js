d0aae9bba7287ea0963a4b712228651464bd8277
Sam Eshun Danquah <62679434+scedanquah@users.noreply.github.com>
2023-03-29 10:58:58 -0400

    will figure out delete later

--- server/routes/annotations.js
+++ server/routes/annotations.js
@@ -2,20 +2,43 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-const Annotation = require('../models/Comment');
+const Annotation = require('../models/Annotation');
 
 //GET all annotations
-router.get("/", async (_req, res) => {
+router.get("/all", async (_req, res) => {
     const notes = await Annotation.find();
     res.send({ notes });
   });
 
 
 //GET annotations by ID
+router.get("/:id", async (_req, res) => {
+  try {
+    const f_annotation = await Annotations.findById(_req.params.id);
+    res.send({f_annotation});
+  } catch (err) {
+    console.error(err);
+    console.log(err);
+  }
+});
 
 //GET all comments from friends
+//not possible
 
-//POST annotations
+//POST annotation
+router.post("/add", async (req, res) => {
+  const { authorID, songID, noteBody, timestamp } = req.body;
+  console.log(req.body);
+  const annotation = await Annotation.create({
+    authorID: authorID,
+    songID: songID,
+    noteBody: noteBody,
+    timestamp: timestamp
+  });
+  res.status(201).send({
+    annotation,
+  });
+});
 
 //DELETE annotation
 
