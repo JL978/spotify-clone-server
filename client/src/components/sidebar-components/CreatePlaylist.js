f550620a79eecdc666a941cda42e33fa7850857b
Victor Del Carpio <vhdelcg@gmail.com>
2023-04-19 00:09:26 -0400

    refactored code for conciseness

--- client/src/components/sidebar-components/CreatePlaylist.js
+++ client/src/components/sidebar-components/CreatePlaylist.js
@@ -1,32 +1,27 @@
-import React, {useContext} from 'react';
-import Icon from '../icons'
-import {LoginContext, MessageContext} from '../../utilities/context'
+import React, { useContext } from "react";
+import Icon from "../icons";
+import { LoginContext, MessageContext } from "../../utilities/context";
 
-//Component a little different than all the other nav items because it is a button and doesnt link to anything
 function CreatePlaylist() {
-    const loggedIn = useContext(LoginContext)
-    const setMessage = useContext(MessageContext)
+  const loggedIn = useContext(LoginContext);
+  const setMessage = useContext(MessageContext);
 
-    return (
-        <>
-        {loggedIn? 
-        <button className="create-button no-outline" onClick={() => setMessage('Oops, it look like I chose not to implement this feature :)')}>
-            <div className="playlist-icon">
-                <Icon name='Create' />
-            </div>
-            <span className="featured-label">Create Playlist</span>
-        </button>
-        :
-        <button className="create-button no-outline" data-tip='create' data-for='tooltip' data-event='click' onClick={() => setMessage('Oops, it look like I chose not to implement this feature :)')}>
-            <div className="playlist-icon">
-                <Icon name='Create' />
-            </div>
-            <span className="featured-label">Create Playlist</span>
-        </button>}
-        
-        
-        </>
-    );
+  return (
+    <button
+      className="create-button no-outline"
+      data-tip={!loggedIn ? "create" : undefined}
+      data-for={!loggedIn ? "tooltip" : undefined}
+      data-event={!loggedIn ? "click" : undefined}
+      onClick={() =>
+        setMessage("Oops, it looks like I chose not to implement this feature :)")
+      }
+    >
+      <div className="playlist-icon">
+        <Icon name="Create" />
+      </div>
+      <span className="featured-label">Create Playlist</span>
+    </button>
+  );
 }
 
 export default CreatePlaylist;
