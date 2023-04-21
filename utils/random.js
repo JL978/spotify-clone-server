9fc577cd1a20816ce32083481470aac29486cd87
Victor Del Carpio <vhdelcg@gmail.com>
2023-04-21 14:56:59 -0400

    refactor

--- utils/random.js
+++ utils/random.js
@@ -1,11 +1,12 @@
-var generateRandomString = function(length) {
-    var text = '';
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const generateRandomString = (length) => {
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let text = '';
   
-    for (var i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
+  
     return text;
 };
-
+  
 module.exports = generateRandomString;
